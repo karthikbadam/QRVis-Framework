f078229109cd9d0178a3c29fbc747f91cb14c797
karthikbadam <sbadam@purdue.edu>
2015-03-15 00:00:51 -0400

    Yelp example

--- examples/js/yelp-business.js
+++ examples/js/yelp-business.js
@@ -3,25 +3,42 @@ function Business(options) {
 
     var _self = this; 
     
-    //_self.allBusiness = [];
+    _self.allBusiness = [];
     _self.allBusinessObject = {};
 
 }
 
-Business.prototype.addBusiness = function (business_id, lat, lon, category1, category2) {
+Business.prototype.addBusiness = function (business_id, lat, lon, category1, category2, rating) {
     
+    var _self = this; 
+
     var business = {};
     business.business_id = business_id;
     business.lat = lat; 
     business.long = long; 
+    business.category1 = category1;
+    business.category2 = category2;
+    business.reviews = [];
+    business.stars = [];
+    business.rating = rating; 
 
     //_self.allBusiness.push(business);
     _self.allBusinessObject.business_id = business;
 }
 
-Business.prototype.addReview = function (business_id, text) {
-
-    _self.allBusinessObject[business_id].review.push(text);
+Business.prototype.addReview = function (business_id, text, stars) {
     
+    var _self = this; 
+    
+    _self.allBusinessObject[business_id].reviews.push(text);
+    _self.allBusinessObject[business_id].stars.push(stars);
+    
+}
+
+Business.prototype.convertToArray = function () {
+
+    var _self = this; 
+    
+    return; 
 }
 
