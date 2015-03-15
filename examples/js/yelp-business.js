6eee93effa2d93bb7091afc44dc5a49426ffd38d
karthikbadam <sbadam@purdue.edu>
2015-03-15 04:42:43 -0400

    Yelp example-treemap

--- examples/js/yelp-business.js
+++ examples/js/yelp-business.js
@@ -1,56 +1,169 @@
-
 function Business(options) {
 
-    var _self = this; 
-    
+    var _self = this;
+
     _self.allBusiness = [];
     _self.allBusinessObject = {};
 
 }
 
-Business.prototype.addBusiness = function (business_id, lat, lon, category1, category2, rating) {
-    
-    var _self = this; 
+Business.prototype.addBusiness = function (business_id, lat, lon, category1, category2, rating, name, review_count) {
+
+    var _self = this;
 
     var business = {};
     business.business_id = business_id;
-    business.lat = lat; 
-    business.lon = lon; 
+    business.lat = lat;
+    business.lon = lon;
     business.category1 = category1;
     business.category2 = category2;
     business.reviews = [];
     business.stars = [];
-    business.rating = rating; 
+    business.rating = rating;
+    business.name = name;
+    business.review_count = review_count;
 
     //_self.allBusiness.push(business);
     _self.allBusinessObject[business_id] = business;
 }
 
 Business.prototype.addReview = function (business_id, text, stars) {
-    
-    var _self = this; 
-    
+
+    var _self = this;
+
     if (business_id && _self.allBusinessObject[business_id]) {
         _self.allBusinessObject[business_id].reviews.push(text);
         _self.allBusinessObject[business_id].stars.push(stars);
     }
-    
+
 }
 
 Business.prototype.convertToArray = function () {
 
-    var _self = this; 
+    var _self = this;
     var keys = Object.keys(_self.allBusinessObject);
-    
+
     for (var i = 0; i < keys.length; i++) {
         var key = keys[i];
-        
+
         var b = _self.allBusinessObject[key];
         _self.allBusiness.push(b);
-        
+
     }
-    
-    
-    return _self.allBusiness; 
+
+
+    return _self.allBusiness;
 }
 
+Business.prototype.createGeoVisualization = function () {
+
+    var _self = this;
+
+    var width = 960,
+        height = 600;
+
+    var formatNumber = d3.format(",.0f");
+
+    var projection = d3.geo.albers()
+        .translate([width / 2, height / 2])
+        .scale([28000])
+        .rotate([108, 0, 0])
+        .translate([1900, -2250]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    var radius = d3.scale.sqrt()
+        .domain([0, 1e6])
+        .range([0, 15]);
+
+    var svg = _self.geosvg = d3.select("#vizdashboard").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    d3.json("data/us-arizona-counties.json", function (error, json) {
+        if (error) return console.error(error);
+
+        svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("fill", "#efefef")
+            .style("stroke-width", "2")
+            .style("stroke", "gray");
+
+        svg.append("g")
+            .attr("class", "bubble")
+            .selectAll("circle")
+            .data(_self.convertToArray())
+            .enter()
+            .append("circle")
+            .attr("transform", function (d) {
+                return "translate(" + projection([d.lon, d.lat]) + ")";
+
+            })
+            .attr("r", function (d) {
+                return 3 + d.review_count / 30;
+
+            })
+            .append("title")
+            .text(function (d) {
+                return d.name;
+            });
+
+        svg.append("text").attr("transform", "translate(10," + (height - 30) + ")")
+            .text("Phoenix, Arizona")
+            .style("font-size", "25px");
+
+    });
+
+
+};
+
+Business.prototype.createTreemap = function () {
+
+    var width = 480,
+        height = 600;
+
+    var treemap = d3.layout.treemap()
+        .size([width, height])
+        .sticky(true)
+        .value(function (d) {
+            return d.size;
+        });
+
+    var div = _self.treemap = d3.select("#vizdashboard").append("div")
+        .attr("class", "treemap")
+        .style("width", width + "px")
+        .style("height", height + "px")
+
+    var node = div.datum(root).selectAll(".treemap-node")
+        .data(treemap.nodes)
+        .enter().append("div")
+        .attr("class", "treemap-node")
+        .call(position)
+        .style("background", function (d) {
+            return d.children ? color(d.name) : null;
+        })
+        .text(function (d) {
+            return d.children ? null : d.name;
+        });
+
+    function position() {
+        this.style("left", function (d) {
+                return d.x + "px";
+            })
+            .style("top", function (d) {
+                return d.y + "px";
+            })
+            .style("width", function (d) {
+                return Math.max(0, d.dx - 1) + "px";
+            })
+            .style("height", function (d) {
+                return Math.max(0, d.dy - 1) + "px";
+            });
+    }
+
+
+};
--- examples/js/yelp-dashboard.js
+++ examples/js/yelp-dashboard.js
@@ -4,9 +4,6 @@ var files = ['data/yelp_training_set_business2.csv', 'data/yelp_training_set_rev
 
 // Business: business_id, categories, review_count, name, longitude, stars, latitude
 
-var width = 960,
-    height = 600;
-
 var business;
 
 $(document).ready(function () {
@@ -47,7 +44,7 @@ $(document).ready(function () {
                         d.latitude = parseFloat(d.latitude);
                         d.longitude = parseFloat(d.longitude);
 
-                        business.addBusiness(d.business_id, d.latitude, d.longitude, d.categories1, d.categories2, d.stars);
+                        business.addBusiness(d.business_id, d.latitude, d.longitude, d.categories1, d.categories2, d.stars, d.name, d.review_count);
 
                         console.log("lat: " + d.latitude + ", long: " + d.longitude);
                     }
@@ -67,62 +64,8 @@ $(document).ready(function () {
 });
 
 function createVisualization() {
-
-    var formatNumber = d3.format(",.0f");
-
-    var projection = d3.geo.albers()
-        .translate([width / 2, height / 2])
-        .scale([28000])
-        .rotate([108, 0, 0])
-        .translate([1900, -2250]);
-
-    var path = d3.geo.path()
-        .projection(projection);
-
-    var radius = d3.scale.sqrt()
-        .domain([0, 1e6])
-        .range([0, 15]);
-
-    var svg = d3.select("#vizdashboard").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    d3.json("data/us-arizona-counties.json", function (error, json) {
-        if (error) return console.error(error);
-
-        svg.selectAll("path")
-            .data(json.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .style("fill", "#efefef")
-            .style("stroke-width", "2")
-            .style("stroke", "gray");
-
-        svg.append("g")
-            .attr("class", "bubble")
-            .selectAll("circle")
-            .data(business.convertToArray())
-            .enter()
-            .append("circle")
-            .attr("transform", function (d) {
-                return "translate(" + projection([d.lon, d.lat]) + ")";
-
-            })
-            .attr("r", 5)
-            .append("title")
-            .text(function (d) {
-                return d.name;
-            });
-        
-        svg.append("text").attr("transform", "translate(10,"+ (height  - 30) +")")
-            .text("Phoenix, Arizona")
-            .style("font-size", "25px");
-
-    });
-
-    //    d3.select(self.frameElement).style("height", height + "px");
-
-
+    
+    business.createGeoVisualization();
+    
 
 }
